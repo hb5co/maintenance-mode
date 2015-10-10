1f271b124c7eb0b347abbd5f21dd8dced820cef8
Bryan Stalcup <bryan@bryanstalcup.com>
2015-10-10 19:26:08 -0400

    add summary and git repo url

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'hb5:maintenance-mode',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Disable access to all content and functionality without logging in using as an administrator.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/hb5co/maintenance-mode.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
