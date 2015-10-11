338cfcc9a53a562db790d7fd1ba3baa199a041fe
Mark Shropshire <mdshrops@shropnet.net>
2015-10-11 01:28:19 -0400

    Setup collection and default document on startup. Closes #9

--- maintenance-mode.js
+++ maintenance-mode.js
@@ -1,1 +1,11 @@
-// Write your package code here!
+// Declare collections.
+MaintMode = new Mongo.Collection('maintenance-mode');
+
+// Startup functions.
+Meteor.startup(function () {
+  // Add default maintenance mode document if missing.
+  if (!MaintMode.find().count()) {
+    MaintMode.insert( { status: false, message: "This site is currently under maintenance. We should be back shortly. Thank you for your patience." } );
+  }
+});
+
